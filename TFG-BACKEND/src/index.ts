8ed8a0b000975500058bf9cdf4fde068a476ec2e
GatoHery <00177919@uca.edu.sv>
2025-11-14 21:02:20 -0600

    test

--- TFG-BACKEND/src/index.ts
+++ TFG-BACKEND/src/index.ts
@@ -1,7 +1,15 @@
 import dotenv from "dotenv";
 import Server from "./models/server";
+import { dbConnection } from "./database/config";
 
 dotenv.config();
 
 const server = new Server();
-server.listen();
+
+// Conectar a la base de datos antes de iniciar el servidor
+dbConnection().then(() => {
+  server.listen();
+}).catch((error) => {
+  console.error("❌ No se pudo conectar a la base de datos:", error);
+  process.exit(1);
+});
