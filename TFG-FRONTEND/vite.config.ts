bc7e441664d008cfdc962cc1b4003b64ad71d2b1
GatoHery <00177919@uca.edu.sv>
2025-11-16 09:56:28 -0600

    test

--- TFG-FRONTEND/vite.config.ts
+++ TFG-FRONTEND/vite.config.ts
@@ -1,25 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-
-  base: '/alarma/',
-
-=======
   server: {
-    host: true,
     port: 5173,
-    watch: {
-      usePolling: true,
-    },
-  },
->>>>>>> parent of 6e9d4d8 (🐛 Create Hosts)
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+    host: '0.0.0.0'
+  }
 })
