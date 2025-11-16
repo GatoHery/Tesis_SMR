2d7eee0ee3db21ef51b7b8bc17920917d3cb3112
GatoHery <00177919@uca.edu.sv>
2025-11-16 10:03:28 -0600

    test

--- TFG-FRONTEND/vite.config.ts
+++ TFG-FRONTEND/vite.config.ts
@@ -5,6 +5,19 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+<<<<<<< HEAD
     host: '0.0.0.0'
   }
 })
+=======
+    watch: {
+      usePolling: true,
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
+>>>>>>> parent of 6e9d4d8 (🐛 Create Hosts)
