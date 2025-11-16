2f507510e6b4b8caf0bd02a8e938a8ba8d47039b
GatoHery <00177919@uca.edu.sv>
2025-11-16 10:24:07 -0600

    test

--- TFG-FRONTEND/vite.config.ts
+++ TFG-FRONTEND/vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    allowedHosts: ['dei.uca.edu.sv', 'localhost', '127.0.0.1']
   }
 })
