b41628ac5280dfd04185033ffcd4bbed3f481801
mhernandez2006 <marcos.hernandez@datalysisgroup.com>
2025-12-01 22:32:40 -0600

    fix
    agregando condición a emisor de recursos para evitar undefined o cualquier otro valor.

--- TFG-BACKEND/src/emitters/resources.emitter.ts
+++ TFG-BACKEND/src/emitters/resources.emitter.ts
@@ -5,17 +5,21 @@ export const resourcesEmitter = {
   emitSimplifiedResources: (io: SocketIOServer, intervalMs = 60000) => {
     setInterval(async () => {
       try {
+        let simplifiedResources: any[] = [];
         const resources = await fetchResources();
+        if (Array.isArray(resources)) {
+          const resourcesInDEI =
+            resources?.filter((resource: any) => resource.location === "DEI") ??
+            [];
 
-        const resourcesInDEI =
-          resources?.filter((resource: any) => resource.location === "DEI") ??
-          [];
-
-        const simplifiedResources = resourcesInDEI.map((resource: any) => ({
-          name: resource.name,
-          location: resource.location,
-          maxParticipants: resource.maxParticipants,
-        }));
+          simplifiedResources = resourcesInDEI.map((resource: any) => ({
+            name: resource.name,
+            location: resource.location,
+            maxParticipants: resource.maxParticipants,
+          }));
+        } else {
+          simplifiedResources = [];
+        }
 
         io.emit("simplified resources", simplifiedResources);
 
